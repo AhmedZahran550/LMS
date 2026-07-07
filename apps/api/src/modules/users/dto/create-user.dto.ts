ddf768ff337467e0dca0a5e9622948d7d01ebc2c
AhmedZahran550 <ahmdzhran50@gmail.com>
2026-07-08 00:40:45 +0300

    feat: add CreateUserDto with validation rules for user registration

--- apps/api/src/modules/users/dto/create-user.dto.ts
+++ apps/api/src/modules/users/dto/create-user.dto.ts
@@ -1,10 +1,15 @@
-import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, MinLength, IsBoolean } from 'class-validator';
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, MinLength, IsBoolean, Matches } from 'class-validator';
 import { UserRole } from '@lms/shared-types';
 
 export class CreateUserDto {
   @IsEmail()
   @IsNotEmpty()
   email!: string;
+
+  @IsString()
+  @IsOptional()
+  @Matches(/^\+[1-9]\d{1,14}$/, { message: 'Mobile number must be in valid E.164 format (e.g. +201234567890)' })
+  mobileNumber?: string;
 
   @IsString()
   @IsNotEmpty()
